1ff54ff0e3fd942607b5416b62d819615b043c5f
taker610 <trennb@gmail.com>
2024-09-27 08:42:44 -0600

    Updated Postgres URI

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-#import os
+import os
 from flask_cors import CORS
 
 app = Flask(__name__)
